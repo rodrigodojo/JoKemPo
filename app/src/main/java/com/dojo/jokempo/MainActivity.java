b535a315c3575d991071708bbee9ae6f0ca7179e
rodrigodojo <rodrigodoj@gmail.com>
2021-02-18 12:06:55 -0300

    criada função de jogada Rival

--- app/src/main/java/com/dojo/jokempo/MainActivity.java
+++ app/src/main/java/com/dojo/jokempo/MainActivity.java
@@ -9,11 +9,14 @@ import android.view.animation.Animation;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity{
 
     ImageView jogador;
     ImageButton buttonRock, buttonScissors, buttonPaper, buttonJogador1, buttonJogador2, buttonVS;
     Animation aparecer1, aparecervs, aparecer2;
+    int num=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +35,8 @@ public class MainActivity extends AppCompatActivity{
         aparecer2 = new AlphaAnimation(0,1);
 
         aparecer1.setDuration(500);
-        aparecervs.setDuration(5000);
+        aparecervs.setDuration(4000);
+        aparecer2.setDuration(6000);
 
         aparecer1.setAnimationListener(new Animation.AnimationListener() {
             @Override
@@ -88,25 +92,56 @@ public class MainActivity extends AppCompatActivity{
 
     public void jogadaBotao(View view){
 
+        buttonJogador1.setScaleX(-1);
+
         switch (view.getId()){
             case(R.id.buttonPedra):
                 buttonJogador1.setImageResource(R.drawable.pedra);
                 buttonVS.setImageResource(R.drawable.vs);
                 buttonJogador1.startAnimation(aparecer1);
                 buttonVS.startAnimation(aparecervs);
+                jogadaRival();
                 break;
             case(R.id.buttonPapel):
                 buttonJogador1.setImageResource(R.drawable.papel);
                 buttonVS.setImageResource(R.drawable.vs);
                 buttonJogador1.startAnimation(aparecer1);
                 buttonVS.startAnimation(aparecervs);
+                jogadaRival();
                 break;
             case(R.id.buttonTesoura):
                 buttonJogador1.setImageResource(R.drawable.tesoura);
                 buttonVS.setImageResource(R.drawable.vs);
                 buttonJogador1.startAnimation(aparecer1);
                 buttonVS.startAnimation(aparecervs);
+                jogadaRival();
                 break;
+
+
+        }
+    }
+
+    public void jogadaRival(){
+        num = new Random().nextInt(4) +1;
+        switch(num){
+            case 1:
+                buttonJogador2.setImageResource(R.drawable.pedra);
+                buttonJogador2.startAnimation(aparecer2);
+                break;
+            case 2:
+                buttonJogador2.setImageResource(R.drawable.papel);
+                buttonJogador2.startAnimation(aparecer2);
+                break;
+            case 3:
+                buttonJogador2.setImageResource(R.drawable.tesoura);
+                buttonJogador2.startAnimation(aparecer2);
+                break;
+
+                default:
+                    buttonJogador2.setImageResource(R.drawable.pedra);
+                    buttonJogador2.startAnimation(aparecer2);
+                    break;
+
 
         }
     }
