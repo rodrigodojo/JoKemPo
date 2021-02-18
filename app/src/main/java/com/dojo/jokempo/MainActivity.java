de2e44f8a811c070f6d1fc5b8bc60c2232bd86da
rodrigodojo <rodrigodoj@gmail.com>
2021-02-18 15:09:32 -0300

    criada função de quemGanhou.

--- app/src/main/java/com/dojo/jokempo/MainActivity.java
+++ app/src/main/java/com/dojo/jokempo/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -16,7 +17,13 @@ public class MainActivity extends AppCompatActivity{
     ImageView jogador;
     ImageButton buttonRock, buttonScissors, buttonPaper, buttonJogador1, buttonJogador2, buttonVS;
     Animation aparecer1, aparecervs, aparecer2;
-    int num=0;
+    int num=0, jogador1=0, jogador2=0;
+    /*
+     0 - estado inicial
+     1 - pedra
+     2 - papel
+     3 - tesoura
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +42,8 @@ public class MainActivity extends AppCompatActivity{
         aparecer2 = new AlphaAnimation(0,1);
 
         aparecer1.setDuration(500);
-        aparecervs.setDuration(4000);
-        aparecer2.setDuration(6000);
+        aparecervs.setDuration(2000);
+        aparecer2.setDuration(3000);
 
         aparecer1.setAnimationListener(new Animation.AnimationListener() {
             @Override
@@ -81,6 +88,7 @@ public class MainActivity extends AppCompatActivity{
             @Override
             public void onAnimationEnd(Animation animation) {
                 buttonJogador2.setVisibility(View.VISIBLE);
+                quemGanhou();
             }
 
             @Override
@@ -96,6 +104,7 @@ public class MainActivity extends AppCompatActivity{
 
         switch (view.getId()){
             case(R.id.buttonPedra):
+                jogador1 = 1;
                 buttonJogador1.setImageResource(R.drawable.pedra);
                 buttonVS.setImageResource(R.drawable.vs);
                 buttonJogador1.startAnimation(aparecer1);
@@ -103,6 +112,7 @@ public class MainActivity extends AppCompatActivity{
                 jogadaRival();
                 break;
             case(R.id.buttonPapel):
+                jogador1 = 2;
                 buttonJogador1.setImageResource(R.drawable.papel);
                 buttonVS.setImageResource(R.drawable.vs);
                 buttonJogador1.startAnimation(aparecer1);
@@ -110,6 +120,7 @@ public class MainActivity extends AppCompatActivity{
                 jogadaRival();
                 break;
             case(R.id.buttonTesoura):
+                jogador1 = 3;
                 buttonJogador1.setImageResource(R.drawable.tesoura);
                 buttonVS.setImageResource(R.drawable.vs);
                 buttonJogador1.startAnimation(aparecer1);
@@ -122,27 +133,36 @@ public class MainActivity extends AppCompatActivity{
     }
 
     public void jogadaRival(){
-        num = new Random().nextInt(4) +1;
+        num = new Random().nextInt(3);
         switch(num){
-            case 1:
+            case 0:
+                jogador2 = 1;
                 buttonJogador2.setImageResource(R.drawable.pedra);
                 buttonJogador2.startAnimation(aparecer2);
                 break;
-            case 2:
+            case 1:
+                jogador2 = 2;
                 buttonJogador2.setImageResource(R.drawable.papel);
                 buttonJogador2.startAnimation(aparecer2);
                 break;
-            case 3:
+            case 2:
+                jogador2 = 3;
                 buttonJogador2.setImageResource(R.drawable.tesoura);
                 buttonJogador2.startAnimation(aparecer2);
                 break;
 
-                default:
-                    buttonJogador2.setImageResource(R.drawable.pedra);
-                    buttonJogador2.startAnimation(aparecer2);
-                    break;
+        }
+    }
 
-
+    public void quemGanhou(){
+        if(jogador1 == jogador2){
+            Toast.makeText(this,"Empate!",Toast.LENGTH_SHORT).show();
+        }
+        if(jogador1 == 1 && jogador2 ==3 || jogador1 == 2 && jogador2 == 1 || jogador1 == 3 && jogador2 == 2){
+            Toast.makeText(this,"Jogador Ganhou",Toast.LENGTH_SHORT).show();
+        }
+        if(jogador1 == 3 && jogador2 ==1 || jogador1 == 1 && jogador2 == 2 || jogador1 == 2 && jogador2 == 3){
+            Toast.makeText(this,"Android Ganhou",Toast.LENGTH_SHORT).show();
         }
     }
 }
